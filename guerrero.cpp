8b636e8dca8922b98c7b4aea0b73a544232c640f
micheldavalos <michel.davalos@academicos.udg.mx>
2018-05-15 12:09:33 -0500

    Creada la clase Guerrero y el enumerado GuerreroTipo

---
+++ guerrero.cpp
@@ -0,0 +1,6 @@
+#include "guerrero.h"
+
+Guerrero::Guerrero()
+{
+
+}
---
+++ guerrero.h
@@ -0,0 +1,11 @@
+#ifndef GUERRERO_H
+#define GUERRERO_H
+
+
+class Guerrero
+{
+public:
+    Guerrero();
+};
+
+#endif // GUERRERO_H
