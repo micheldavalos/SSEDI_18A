de1fb27fcb8504e1210b9b62122ba765ffbb251e
Michel Dávalos <michel.davalos@academicos.udg.mx>
2018-05-25 23:53:04 -0500

    creada en clase civilizacion la cola de prioridad para los recursos

--- Recurso.cpp
+++ Recurso.cpp
@@ -3,3 +3,32 @@
 //
 
 #include "Recurso.h"
+
+Recurso::Recurso()
+{
+
+}
+
+const string &Recurso::getId() const {
+    return id;
+}
+
+void Recurso::setId(const string &id) {
+    Recurso::id = id;
+}
+
+size_t Recurso::getCantidad() const {
+    return cantidad;
+}
+
+void Recurso::setCantidad(size_t cantidad) {
+    Recurso::cantidad = cantidad;
+}
+
+TipoRecurso Recurso::getTipo() const {
+    return tipo;
+}
+
+void Recurso::setTipo(TipoRecurso tipo) {
+    Recurso::tipo = tipo;
+}
--- Recurso.h
+++ Recurso.h
@@ -5,8 +5,34 @@
 #ifndef SSEDI_18A_RECURSO_H
 #define SSEDI_18A_RECURSO_H
 
+#include <iostream>
+using namespace std;
+
+enum class TipoRecurso {ORO, MADERA, PIEDRA, COMIDA};
 
 class Recurso {
+
+private:
+    string id;
+    size_t cantidad;
+    TipoRecurso tipo;
+
+public:
+
+    Recurso();
+
+    const string &getId() const;
+
+    void setId(const string &id);
+
+    size_t getCantidad() const;
+
+    void setCantidad(size_t cantidad);
+
+    TipoRecurso getTipo() const;
+
+    void setTipo(TipoRecurso tipo);
+
 
 };
 
