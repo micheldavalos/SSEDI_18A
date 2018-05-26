2b30bce1737d9f4a07a238740cbec89c25ecb0c2
Michel Dávalos <michel.davalos@academicos.udg.mx>
2018-05-25 23:43:35 -0500

    creada la clase Recurso

---
+++ Recurso.cpp
@@ -0,0 +1,5 @@
+//
+// Created by michel on 5/25/18.
+//
+
+#include "Recurso.h"
---
+++ Recurso.h
@@ -0,0 +1,14 @@
+//
+// Created by michel on 5/25/18.
+//
+
+#ifndef SSEDI_18A_RECURSO_H
+#define SSEDI_18A_RECURSO_H
+
+
+class Recurso {
+
+};
+
+
+#endif //SSEDI_18A_RECURSO_H
